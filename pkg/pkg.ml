9d742c41cf0d4f8d2734b2d51731e0ec5f8beabf
Federico Gimenez <fgimenez@coit.es>
2016-12-04 15:53:31 +0100

    ported to topkg

---
+++ pkg/pkg.ml
@@ -0,0 +1,12 @@
+#!/usr/bin/env ocaml
+#use "topfind"
+#require "topkg"
+open Topkg
+
+let () =
+  Pkg.describe "io-page" @@ fun c ->
+  Ok [ Pkg.mllib "lib/uri.mllib";
+       Pkg.mllib "lib/services.mllib";
+       Pkg.mllib "lib/services_full.mllib";
+       Pkg.mllib "top/uri_top.mllib";
+       Pkg.test "lib_test/test_runner" ]
